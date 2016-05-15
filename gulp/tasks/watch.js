f50e2b31c48d0fc66050a4193bf07dcea6a09348
tatsuya azegami <theorthodoxworks@gmail.com>
2016-05-16 02:49:44 +0900

    Update imagemin

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,4 +5,5 @@ var config = require('../config');
 gulp.task('watch', function() {
   gulp.watch(config.sass.watch, ['sass']);
   gulp.watch(config.js.watch, ['js']);
+  gulp.watch(config.images.watch, ['imagemin']);
 });
