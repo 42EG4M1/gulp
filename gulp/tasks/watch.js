eeca57c9c8d7167ca56b33893328df047f6185dc
tatsuya azegami <theorthodoxworks@gmail.com>
2016-05-17 14:48:30 +0900

    Add gulp-watch

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,9 +1,10 @@
 var gulp   = require('gulp');
 var config = require('../config');
+var $      = require('gulp-load-plugins')();
 
 
 gulp.task('watch', function() {
-  gulp.watch(config.sass.watch, ['sass']);
-  gulp.watch(config.js.watch, ['js']);
-  gulp.watch(config.images.watch, ['imagemin']);
+  $.watch(config.sass.watch, ['sass']);
+  $.watch(config.js.watch, ['js']);
+  $.watch(config.images.watch, ['imagemin']);
 });
