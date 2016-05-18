007ac23c1390506f8fa22eb80c28a8995c80ab2e
tatsuya azegami <theorthodoxworks@gmail.com>
2016-05-19 02:46:22 +0900

    gulp-watchが動いていなかったので修正

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,7 +4,13 @@ var $      = require('gulp-load-plugins')();
 
 
 gulp.task('watch', function() {
-  $.watch(config.sass.watch, ['sass']);
-  $.watch(config.js.watch, ['js']);
-  $.watch(config.images.watch, ['imagemin']);
+  $.watch(config.sass.watch, function(event) {
+    gulp.start(['sass']);
+  });
+  $.watch(config.js.watch, function(event) {
+    gulp.start(['js']);
+  });
+  $.watch(config.images.watch, function(event) {
+    gulp.start(['imagemin']);
+  });
 });
