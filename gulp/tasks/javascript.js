dfb993a0681339e593dc98ce86818cd2e4a3f09c
tatsuya azegami <theorthodoxworks@gmail.com>
2016-05-22 14:40:10 +0900

    jqueryを追加してconcat

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -3,9 +3,19 @@ var config = require('../config').js;
 var $      = require('gulp-load-plugins')();
 
 
-gulp.task('js', function() {
+gulp.task('js', ['uglify'], function() {
+  return gulp.src([
+    'node_modules/jquery/dist/jquery.min.js',
+    config.dest + 'app.min.js'
+  ])
+    .pipe($.concat('app.min.js'))
+    .pipe(gulp.dest(config.dest));
+});
+
+gulp.task('uglify', function() {
   return gulp.src(config.src)
     .pipe($.plumber())
     .pipe($.uglify())
+    .pipe($.rename('app.min.js'))
     .pipe(gulp.dest(config.dest));
 });
